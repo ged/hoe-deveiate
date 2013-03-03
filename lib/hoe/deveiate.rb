2f1f242691d6b89806c9ee05fc6268c7279bd69a
Michael Granger <ged@FaerieMUD.org>
2013-03-03 10:19:49 -0800

    Bump minor version, update history.

--- lib/hoe/deveiate.rb
+++ lib/hoe/deveiate.rb
@@ -14,7 +14,7 @@ Hoe.plugin( :highline, :mercurial )
 module Hoe::Deveiate
 
 	# Library version constant
-	VERSION = '0.1.1'
+	VERSION = '0.2.0'
 
 	# Version-control revision constant
 	REVISION = %q$Revision$
